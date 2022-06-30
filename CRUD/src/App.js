19717b4e139a97ad9c765a952e2eae1739da5752
ikeadely <dwike.adelya@gmail.com>
2022-06-30 10:00:51 +0700

    consum API

---
+++ CRUD/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+// import Get from './Get';
+// import { Login } from './Component/Form/Login';
+
+import { Home } from './Component/Pages/Home';
+// import { Form } from 'antd';
+
+function App() {
+  return (
+  <div className="App">
+    <Home/>
+  </div>
+  );
+}
+
+export default App;
